a2fd5759d2750b2879499ec45844ca5ddb5e47d8
Piyumal <139644276+AwishkaPiyumal@users.noreply.github.com>
2025-05-30 10:22:42 +0530

    Update DepartmentRepo.java

--- src/main/java/com/example/demo/repo/DepartmentRepo.java
+++ src/main/java/com/example/demo/repo/DepartmentRepo.java
@@ -1,11 +1,19 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Department;
 
 @Repository
 public interface DepartmentRepo extends JpaRepository<Department, Integer> {
+    
+    @Query("SELECT name FROM Department")
+    public List<String>getDeptNames();
 
+    @Query("SELECT d From Department d WHERE d.name LIKE %?1%")
+    public List<Department> searchName(String name);
 }
