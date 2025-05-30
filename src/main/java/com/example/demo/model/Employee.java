353b366bf285e152b07d7d8aebc57d2544fb2d70
Piyumal <139644276+AwishkaPiyumal@users.noreply.github.com>
2025-05-30 09:31:23 +0530

    Update Employee.java

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
@@ -18,4 +16,40 @@ public class Employee extends Person {
 	private Department department;
 	@ManyToMany(mappedBy = "employees")
 	private List<Project>projects;
+	public String getEmpNo() {
+		return empNo;
+	}
+
+	public Employee(String name, int age, String gender, String empNo, double salary, Department department,
+	List<Project> projects) {
+		super(name, age, gender);
+		this.empNo = empNo;
+		this.salary = salary;
+		this.department = department;
+		this.projects = projects;
+	}
+
+
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public List<Project> getProjects() {
+		return projects;
+	}
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+	
 }
