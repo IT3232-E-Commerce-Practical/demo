8d1dc744025045cd26eed954c79b7939e07f2903
Piyumal <139644276+AwishkaPiyumal@users.noreply.github.com>
2025-05-30 10:22:46 +0530

    Update DepartmentService.java

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -22,5 +23,28 @@ public class DepartmentService {
 			throw new EntityNotFoundException("Department Not Found");
 		}
 		return repo.findById(id).get();
+	}
+
+	public String addDept(Department department){
+		if (repo.findById(department.getId()).isPresent()){
+			throw new DuplicateKeyException("The department is alredy available");
+		}
+		repo.save(department);
+		return "New department added";
+	}
+
+	public List<String>getDepartmentNames(){
+		if (repo.getDeptNames().isEmpty()) {
+			throw new EntityNotFoundException("Department Not Found");
+		}
+		return repo.getDeptNames();
+	}
+
+	
+	public List<Department> searchDepartmentByName(String name){
+		if (repo.searchName(name).isEmpty()) {
+			throw new EntityNotFoundException("Department Not Found");
+		}
+		return repo.searchName(name);
 	}
 }
